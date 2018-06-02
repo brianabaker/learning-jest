2bb00d33cf85ec15b9e4e442601c70970257603f
brianabaker <brianaclairbaker@gmail.com>
2018-06-01 21:17:35 -0400

    enzyme working

--- src/Part_2/api.js
+++ src/Part_2/api.js
@@ -1,6 +1,5 @@
 
 // to test, you need the api call to be in a separate file.
-// then, you need to mock the function that's in this file.
 
 const urlPartOne = "http://api.giphy.com/v1/gifs/search?q="
 
@@ -8,6 +7,7 @@ const urlPartTwo = "&api_key=UE0dCN2WofIwVF0RPbpHo0Lz0k9VhqdG"
 
 const gifApi = (query) => {
   return fetch(urlPartOne + query + urlPartTwo)
+  .then(res => res.json())
 }
 
 export {gifApi}
